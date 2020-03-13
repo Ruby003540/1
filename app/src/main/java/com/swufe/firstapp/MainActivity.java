9108f417ff2891ad87afc2f0255ec68b5a296a1e
likai0607 <1349258896@qq.com>
2020-03-13 21:13:46 +0800

    add something ....

--- app/src/main/java/com/swufe/firstapp/MainActivity.java
+++ app/src/main/java/com/swufe/firstapp/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 //add something...
+    //add something...
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
